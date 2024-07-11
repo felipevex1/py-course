c71769293877336313911b7be406743dad7c9827
Felipe Ribeiro <felipe-psn-17@hotmail.com>
2024-07-11 14:30:29 -0300

    If, elif e else

---
+++ if-elif-else.py
@@ -0,0 +1,11 @@
+# if / elif / else
+# se / se nao se / se nao
+
+entrada = input('Você quer "entrar" ou "sair"?')
+
+if entrada == 'entrar':
+    print('Entrou.')
+elif entrada == 'sair':
+    print('Saiu.')
+else:
+    print('Comando não reconhecido.')
